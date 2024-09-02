eb506b342e28d055354cc6193c374855a8acefe0
fathysamir <01154857080fa@gmail.com>
2024-09-02 17:49:09 +0300

    Initial commit

--- app/Console/Commands/WebSocketServer.php
+++ app/Console/Commands/WebSocketServer.php
@@ -11,7 +11,7 @@ use App\WebSockets\Chat;
 
 class WebSocketServer extends Command
 {
-    protected $signature = 'websockets:serve';
+    protected $signature = 'websocket:init';
     protected $description = 'Start the WebSocket server';
 
     public function __construct()
