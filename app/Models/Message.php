b86329d02b7d38c4e0c6663fc715e4e4cb5479a7
fathysamir <01154857080fa@gmail.com>
2024-09-11 12:24:46 +0300

    Initial commit

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,43 +4,66 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\Crypt;
 class Message extends Model
 {
     use HasFactory;
-
     protected $table = 'messages';
-    protected $fillable = ['sender_id', 'receiver_id', 'message', 'path', 'type', 'location_link'];
-    protected $allowedSorts = ['created_at', 'updated_at'];
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'message',
+        'path',
+        'type',
+        'location_link'
+    ];
+    protected $allowedSorts = [
+       
+        'created_at',
+        'updated_at'
+    ];
     protected $guarded = [];
-
     public function getMessageAttribute($value)
-    {
-        return $value ? decrypt($value) : null;
+    {   
+        if($value!=null){
+        return Crypt::decryptString($value);
+        }else{
+            return null;
+        }
+        
     }
 
     public function setMessageAttribute($value)
     {
-        $this->attributes['message'] = encrypt($value);
+        $this->attributes['message'] = Crypt::encryptString($value);
     }
 
     public function getLocationLinkAttribute($value)
     {
-        return $value ? decrypt($value) : null;
+        if($value!=null){
+            return Crypt::decryptString($value);
+            }else{
+                return null;
+            }
     }
 
     public function setLocationLinkAttribute($value)
     {
-        $this->attributes['location_link'] = encrypt($value);
+        $this->attributes['location_link'] = Crypt::encryptString($value);
     }
 
     public function getPathAttribute($value)
     {
-        return $value ? decrypt($value) : null;
+        if($value!=null){
+            return Crypt::decryptString($value);
+            }else{
+                return null;
+            }
     }
 
     public function setPathAttribute($value)
     {
-        $this->attributes['path'] = encrypt($value);
+        $this->attributes['path'] = Crypt::encryptString($value);
     }
+    
 }
